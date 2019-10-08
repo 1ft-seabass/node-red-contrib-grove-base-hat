619787e3d8af55bce8c9e1bf2dd35008e26c7e3b
Tanaka Seigo <seigo@1ft-seabass.jp>
2019-10-08 10:39:30 +0900

    Has been changed grove-oled-display-128x64 node receive simple string data.

--- grove-oled-display-128x64/grove-oled-display-128x64.js
+++ grove-oled-display-128x64/grove-oled-display-128x64.js
@@ -11,10 +11,11 @@ module.exports = function(RED) {
         node.on('input', function(msg) {
             // msg.payload = msg.payload.toLowerCase();
             const exec = require('child_process').exec;
-            const message = msg.payload.message;
+            // const message = msg.payload.message;
+            const message = msg.payload;  // Changed simple string payload in v.0.0.4.
             //console.log(clear);
             this.status({fill:"yellow",shape:"ring",text:this.port_name + " connecting"});
-            exec('python -u ' + path.join( __dirname , 'grove-oled-display-128x64.py') + ' ' + message, (err, stdout, stderr) => {
+            exec('python -u ' + path.join( __dirname , 'grove-oled-display-128x64.py') + ' "' + message + '"', (err, stdout, stderr) => {
                 if (err) { console.log(err); }
                 // console.log(stdout);
                 this.status({fill:"green",shape:"dot",text:this.port_name + " connected"});
