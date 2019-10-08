a1848a2e6e0bb00fab472d2a31fe51ff59763cb6
Tanaka Seigo <seigo@1ft-seabass.jp>
2019-10-08 10:43:22 +0900

    Added grove-temperature-humidity-sensor-sht3x node.

---
+++ grove-temperature-humidity-sensor-sht3x/grove-temperature-humidity-sensor-sht3x.js
@@ -0,0 +1,26 @@
+var path = require('path');
+
+module.exports = function(RED) {
+    function GroveTemperatureHumiditySensorSHT3x(config) {
+        RED.nodes.createNode(this,config);
+        
+        this.port_name = "I2C";
+        this.status({fill:"blue",shape:"dot",text:this.port_name});
+
+        var node = this;
+        node.on('input', function(msg) {
+            const exec = require('child_process').exec;
+            this.status({fill:"yellow",shape:"ring",text:this.port_name + " connecting"});
+            exec('python -u ' + path.join( __dirname , 'grove-temperature-humidity-sensor-sht3x.py' ), (err, stdout, stderr) => {
+                if (err) { console.log(err); }
+                // console.log(stdout);
+                msg.payload = JSON.parse(stdout);
+                msg.payload.temperature = Number(msg.payload.temperature);
+                msg.payload.humidity = Number(msg.payload.humidity);
+                this.status({fill:"green",shape:"dot",text:this.port_name + " connected"});
+                node.send(msg);
+            });
+        });
+    }
+    RED.nodes.registerType("grove-temperature-humidity-sensor-sht3x",GroveTemperatureHumiditySensorSHT3x);
+}
